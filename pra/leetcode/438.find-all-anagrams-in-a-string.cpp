c9a0706a210838dfa453e407ea5f0a1b327394b1
onecreeper <liuzexi200709@126.com>
2025-03-18 18:29:33 +0800

    保存下

--- pra/leetcode/438.find-all-anagrams-in-a-string.cpp
+++ pra/leetcode/438.find-all-anagrams-in-a-string.cpp
@@ -38,34 +38,58 @@ using namespace std;
 class Solution {
     public:
         vector<int> findAnagrams(string s, string p) {
-            vector<int> counted(26, 0);
-            vector<int> toFind(26, 0);
+            vector<int> counted(26,0);
+            vector<int> toFind(26,0);
             vector<int> res;
             int n = s.size();
             int m = p.size();
-            
-            if (n < m) return res;
-    
-            for (auto i : p) toFind[int(i) - int('a')]++;
-            for (int i = 0; i < m; ++i) {
-                counted[int(s[i]) - int('a')]++;
-            }
-            if (counted == toFind) res.push_back(0);
-            for (int i = m; i < n; ++i) {
-                counted[int(s[i]) - int('a')]++;
-                counted[int(s[i - m]) - int('a')]--;
-                if (counted == toFind) {
-                    res.push_back(i - m + 1);
-                }
-            }
-            
-            return res;
-        }
-    };
+            bool isTarget = true;
 
+            for(auto i : p) toFind[int(i) - int('a')] = i;
+
+            for (int i = 0; i < n;++i){
+                isTarget = true;
+
+                for(int j = i; j < n;++j){
+                    if ((toFind[int(j) - int('a')] == 0) || (j - i > m) || isTarget == false) break;
+                    ++counted[int(j) - int('a')];
+                    if (toFind[int(j) - int('a')] == counted[int(j) - int('a')]){
+                        for(int k = 0;k < 26;++k){
+                            if(toFind[k] != counted[k]){
+                                isTarget = false;
+                                break;
+                            }
+                        } 
+                    }
+                }
+
+                if (isTarget == true) res.push_back(i);
+            }
+            return res;
+    }
+};
+
+
+class Solution2 {
+    public:
+        vector<int> findAnagrams(string s, string p) {
+            vector<int> toFind(26,0);
+            vector<int> res;
+            int n = s.size() , m = p.size();
+            for (auto i : p) ++toFind[int(i) - int('a')]; 
+            for (int i = 0 ; i <= n - m; ++i){
+                vector<int> counted(26,0);
+                for (int j = i ; j < i + m ; ++j){
+                    ++counted[int(s[j]) - int('a')];
+                }
+                if (counted == toFind) res.push_back(i);
+            }
+            return res;    
+    }
+};
 
 int main() {
-    Solution s;
+    Solution2 s;
     vector<int> res = s.findAnagrams("cbaebabacd","abc");
     for (auto i : res) cout << i;    
 }
