b5d4d302354711a5e2ad3ea64f9d8359927a379e
onecreeper <liuzexi200709@126.com>
2025-03-20 18:38:42 +0800

    add todo

---
+++ pra/leetcode/76.minimum-window-substring.cpp
@@ -0,0 +1,83 @@
+// 万能头文件
+// #include <bits/stdc++.h>  //导致无提示
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <list>
+#include <numeric>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+#include <bitset>
+#include <functional>
+#include <utility>
+#include <sstream>
+#include <iomanip>
+#include <fstream>
+#include <iterator>
+#include <tuple>
+#include <limits>
+#include <ctime>
+#include <random>
+#include <chrono>
+
+
+using namespace std;
+
+// 76. 最小覆盖子串
+// 困难
+// 相关标签
+// 相关企业
+// 提示
+// 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+// 注意：
+// 对于 t 中重复字符，我们寻找的子字符串中该字符数量必须不少于 t 中该字符数量。
+// 如果 s 中存在这样的子串，我们保证它是唯一的答案。
+// 示例 1：
+// 输入：s = "ADOBECODEBANC", t = "ABC"
+// 输出："BANC"
+// 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+// 示例 2：
+// 输入：s = "a", t = "a"
+// 输出："a"
+// 解释：整个字符串 s 是最小覆盖子串。
+// 示例 3:
+// 输入: s = "a", t = "aa"
+// 输出: ""
+// 解释: t 中两个字符 'a' 均应包含在 s 的子串中，
+// 因此没有符合条件的子字符串，返回空字符串
+
+class Solution1 {
+    public:
+        string minWindow(string s, string t) {
+            
+        }
+    };
+
+
+
+int main() {
+    Solution1 s;
+    string a = "hello", b = ""
+    string res = s.minWindow();
+    cout << res << endl;
+}
+
+
+
+// int main() {
+//     vector<type> nums = {9,9,9,9};
+//     Solution1 s;
+//     type a = s.method(nums);
+//     cout << a << endl;
+// }
